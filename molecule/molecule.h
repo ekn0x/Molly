1df8f6af41a9ce8078d34b69203115f43c2f219c
Alex H.Lamarche <alex.hlamarche@gmail.com>
2016-03-31 10:43:45 -0400

    Update molecule.h

--- molecule/molecule.h
+++ molecule/molecule.h
@@ -14,73 +14,103 @@ class bond;
 class molecule
 {
 private:
-	unsigned int b_id;
-	unsigned int a_id;
-	std::string Name;
-	std::string Formula;
+	unsigned int b_id;	//< key increment for bonds
+	unsigned int a_id;	//< key increment for atoms
+	std::string Name;	//< Name of the molecule
+	bool Formula;		//< either true or false if the formula is up to date
+	std::string Formula;	//< Formula of the molecule
 
-	vect0r<atom*> Atoms;
+	vect0r<atom*> Atoms;	//< Array of atoms
+
+	//
+	// Private Methods
+	//
+	
+	/**
+	 * \Brief Count the number of atoms of one kind
+	 * \Param (std::string) Kind of element to count
+	 * \Return (unsigned int)
+	 */
+	unsigned int countAtoms(std::string)
 
 public:
 	/**
-	 * molecule()
-	 * \Purpose: Basic constructor
-	 * \Parameters: None
-	 * \Returns: None
+	 * \Brief Basic constructor
+	 * \Param None
+	 * \Return: None
 	 */
 	molecule();
 	/**
-	 * ~molecule()
-	 * \Purpose: basic destructor
- 	 * \Parameters: None
-	 * \Returns: None
+	 * \Brief Constructor from file stream
+	 * \Param path to the file to open
+	 * \Return None
+	 */
+	molecule(std::string);
+	/**
+	 * \Brief Destructor
+ 	 * \Param None
+	 * \Return None
 	 */
 	~molecule();
 
+	//
+	// Atom related methods
+	//
+	
 	/**
-	 * display()
-	 * \Purpose: print the information relative to the molecule
-	 * \Parameters: None
-	 * \Returns: None
+	 * \Brief Display one Atom and it's entourage
+	 * \Param (unsigned int) node Id
+	 * 	  (unsigned int) level of entourage
+	 * \Return None
 	 */
-	void display();
+	void displayAtom(unsigned int, unsigned int);
 	/**
-	 * 
-	 * \
-	 * \
-	 * \
+	 * \Brief List atoms in the molecule
+	 * \Param None
+	 * \Return None
 	 */
 	void listAtom();
 	/**
-	 * insertAtom(std::string)
 	 * \Purpose: add an atom to the molecule
 	 * \Parameters: Symbol of the element
 	 * \Returns: None
 	 */
 	bool insertAtom(std::string);
+	
+	//
+	// Bond related methods
+	//
+	
+	/**
+	 * \Brief Joint two atom
+	 * \Param The IDs of the atoms that need to be joint
+	 * \Return None
+	 */
+	void insertBond(unsigned int, unsigned int);
 
 	/**
-	 * insertBond(unsigned int, unsigned int)
-	 * \Purpose: Joint two atom
-	 * \Parameters: The IDs of the atoms that need to be joint
-	 * \Returns: None
+	 * \Brief Remove a link that joint two atom together
+	 * \Param The IDs of the atoms.
+	 * \Return None
 	 */
-	//void insertBond(unsigned int, unsigned int);
-
+	void removeBond(unsigned int, unsigned int);
 	/**
-	 * removeBond(unsigned int, unsigned int)
-	 * \Purpose: Remove a link that joint two atom together
-	 * \Parameters: The IDs of the atoms.
-	 * \Returns: None
-	 */
-	//void removeBond(unsigned int, unsigned int);
-	/**
-	* removeBond(unsigned int)
-	* \Purpose: Remove a link that joint two atom together
-	* \Parameters: The id of the bond
-	* \Returns: None
+	* \Brief Remove a link that joint two atom together
+	* \Param The id of the bond
+	* \Return None
 	*/
-	//void removeBond(unsigned int);
+	void removeBond(unsigned int);
+
+	//
+	// General methods
+	//
+	
+	/**
+	 * \Brief Print the information relative to the molecule
+	 * \Param None
+	 * \Return None
+	 */
+	void display();
 
 };
 
