5a0dfba441a7d469b3654cd2d7b0cddddd1446fc
Alex H.Lamarche <alex.hlamarche@gmail.com>
2016-03-31 09:27:25 -0400

    Update atom.h

--- molecule/atom/atom.h
+++ molecule/atom/atom.h
@@ -1,1 +1,63 @@
+#ifndef _ATOM_H_
+#define _ATOM_H_
 
+#include "element.h"
+#include "vect0r.h"
+
+class element;
+class bond;
+
+/**
+ * \Brief Molecule container
+ * This object provides an interface to manipulate a molecule.
+ */
+class atom
+{
+private:
+	unsigned int id;  //< Unique ID of the atom
+	element* Identity; //< Pointer to an element
+
+	vect0r<bond*> bonds; //< Array of bonds
+
+public:
+	/**
+	 * \Brief Basic constructor
+	 * \Param None
+	 * \Return None
+	 */
+	atom();
+	/**
+	 * atom()
+	 * \Brief Constructor with element and id
+	 * \Param pointer to the kind of element, and id of the atom
+	 * \Return None
+	 */
+	atom(element*, unsigned int);
+	/**
+	 * ~atom()
+	 * \Brief basic destructor
+	 * \Param None
+	 * \Return None
+	 */
+	~atom();
+
+	/**
+	 * insertBond()
+	 * \Brief Add a new connection to the atom
+	 * \Param (bond*) pointer to a bond
+	 * \Return bool (either if the operation succeeded or not)
+	 */
+	bool insertBond(bond*);
+	/**
+	 * display()
+	 * \Brief print the information relative to the atom
+	 * \Param None
+	 * \Return None
+	 */
+	void display();
+
+	inline unsigned int getId() { return id; };
+	inline std::string getElementName() { return Identity->getName; };
+};
+
+#endif
